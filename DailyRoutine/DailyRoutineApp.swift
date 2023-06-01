d4152629405dfaa69a1113505fb9757eabcfdb57
Padoari <padoari85@gmail.com>
2023-06-01 12:17:27 +0900

    Initial Commit

---
+++ DailyRoutine/DailyRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRoutineApp.swift
+//  DailyRoutine
+//
+//  Created by jeongyun on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct DailyRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
