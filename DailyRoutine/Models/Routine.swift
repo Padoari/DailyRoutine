2a1f682440abbdda3562f5fbebe3e949f99f8e14
Padoari <padoari85@gmail.com>
2023-06-02 09:49:51 +0900

    Add Routine

---
+++ DailyRoutine/Models/Routine.swift
@@ -0,0 +1,20 @@
+//
+//  Routine.swift
+//  DailyRoutine
+//
+//  Created by jeongyun on 2023/06/01.
+//
+
+import Foundation
+
+struct Routine{
+    var title: String
+    var isDoneMon: Bool
+    var isDoneTue: Bool
+    var isDoneWed: Bool
+    var isDoneThu: Bool
+    var isDoneFri: Bool
+    var isDoneSat: Bool
+    var isDoneSun: Bool
+    var targetCount: Int
+}
